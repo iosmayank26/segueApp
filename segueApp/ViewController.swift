c046d441348e8de342799021ea30b20977d249fa
Mayank Gupta <mayankIndia@Mayanks-MacBook-Air.local>
2017-09-10 00:36:24 +0530

    Segue fund

--- segueApp/ViewController.swift
+++ segueApp/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Button(_ sender: Any) {
+        
+        performSegue(withIdentifier: "segue1", sender: nil)
+        
+        
+    }
 
 }
 
